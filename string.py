4bf76c1a08fb50800933761a34a2b638dac10554
NidhitheCoder <nidhpv@gmail.com>
2021-11-07 20:40:41 +0530

    Strings in python

---
+++ string.py
@@ -0,0 +1,22 @@
+course = "Python programming"
+print(len(course))
+# Print a charactor by index
+print(course[0])
+print(course[17])
+
+# Print a charactor by index from last.
+print(course[-1])
+print(course[-18])
+
+# Print a substring by start and last index
+print(course[0:4])
+print(course[:4])
+
+# To print entire string
+print(course)
+print(course[0:])
+print(course[:])
+
+# The location
+print(id(course))
+print(id(course[3]))
