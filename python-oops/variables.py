4010f28a6c1f3a96c7d562f3da972c2f04022d7e
NidhitheCoder <nidhpv@gmail.com>
2021-11-20 00:10:57 +0530

    fix: variable types notes

--- python-oops/variables.py
+++ python-oops/variables.py
@@ -1,4 +1,5 @@
 class car:
+  wheels = 4 # class variables
   def __init__(self):
     self.mil = 10  # instance variable
     self.com = "BMW"  # instance variable
@@ -8,5 +9,7 @@ c2 = car()
 
 c1.mil =12
 
-print(c1.com, c1.mil)
-print(c2.com, c2.mil)
+car.wheels = 6
+
+print(c1.com, c1.mil, c1.wheels)
+print(c2.com, c2.mil, c2.wheels)
