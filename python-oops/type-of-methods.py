e8df7ae39f18db8197228413cb2917ca15ebe404
NidhitheCoder <nidhpv@gmail.com>
2021-11-27 14:15:47 +0530

    feat: class variables

--- python-oops/type-of-methods.py
+++ python-oops/type-of-methods.py
@@ -8,12 +8,24 @@ class Student:
   def avg(self):
     return self.m1 + self.m2 + self.m3 / 3
 
-  def get_m1(self):
+  def get_m1(self): 
     return self.m1
+
+  def set_m1(self, value):
+    self.m1 = value
+
+  @classmethod    # decodeters
+  def getSchool(cls):
+    return cls.school
 
 s1 = Student(2, 3, 5)
 s2 = Student(12, 34, 22)
 
+print(s1.m1) # get value of m1 using variables.
+print(s1.get_m1()) # get value of m1 using methods.
+
 print(s1.avg())
 
 # m1. m2, m3 are instance variables
+
+print(Student.getSchool())
