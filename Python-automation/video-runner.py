72b924d22abbd536460115c9a6c63d06fe98fd93
NidhitheCoder <nidhpv@gmail.com>
2022-02-19 23:59:46 +0530

    feat: install chrome f=dirver and selenium

--- Python-automation/video-runner.py
+++ Python-automation/video-runner.py
@@ -3,8 +3,8 @@ from selenium import webdriver
 driver = webdriver.Chrome()
 driver.get('http://youtube.com')
 
-searchbox = driver.find_element('//*[@id="search"]')
+searchbox = driver.find_element_by_css_selector('ytd-searchbox')
 searchbox.send_keys('Python advanced')
 
-searchButton = driver.find_element('//*[@id="search-icon-legacy"]')
-searchbox.click()
+# searchButton = driver.find_element('//*[@id="search-icon-legacy"]')
+# searchbox.click()
