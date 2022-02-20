db0d920e938009927afcc1f97ae61a2aca6e53fb
NidhitheCoder <nidhpv@gmail.com>
2022-02-20 23:59:07 +0530

    feat: add waiting time and trying to fill input using selenium

--- Python-automation/video-runner.py
+++ Python-automation/video-runner.py
@@ -3,8 +3,12 @@ from selenium import webdriver
 driver = webdriver.Chrome()
 driver.get('http://youtube.com')
 
-searchbox = driver.find_element_by_css_selector('ytd-searchbox')
-searchbox.send_keys('Python advanced')
+searchbox = driver.find_element('id',"search")
+driver.implicitly_wait(10)
+print(searchbox);
+searchbox.set_attribute('value', 'Python advanced')
 
 # searchButton = driver.find_element('//*[@id="search-icon-legacy"]')
 # searchbox.click()
+
+# ActionChains(driver).move_to_element(button).click(button).perform()
